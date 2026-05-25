4d3d745b51f78524c723823e97a51d63797923ff
BISAYOT <bisayotduligas@gmail.com>
2026-05-25 11:19:45 +0800

    database

--- backend/app/Config/Database.php
+++ backend/app/Config/Database.php
@@ -1,14 +1,65 @@
 <?php
-// database.php
-// Modify these values for your local XAMPP MySQL setup.
-$DB_HOST = 'localhost';
-$DB_NAME = 'gad_submission_system';
-$DB_USER = 'root';
-$DB_PASS = '';
-$DB_PORT = 3308; // Default XAMPP MySQL port is 3306, change if needed.
 
-$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
-if ($mysqli->connect_error) {
-    die('Database connection error: ' . $mysqli->connect_error);
+namespace Config;
+
+use CodeIgniter\Database\Config;
+
+/**
+ * Database Configuration
+ */
+class Database extends Config
+{
+    public string $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
+    public string $defaultGroup = 'default';
+
+    public array $default = [
+        'DSN'      => '',
+        'hostname' => 'localhost',
+        'username' => '',
+        'password' => '',
+        'database' => '',
+        'DBDriver' => 'MySQLi',
+        'DBPrefix' => '',
+        'pConnect' => false,
+        'DBDebug'  => true,
+        'charset'  => 'utf8mb4',
+        'DBCollat' => 'utf8mb4_general_ci',
+        'swapPre'  => '',
+        'encrypt'  => false,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+        'port'     => 3306,
+    ];
+
+    public array $tests = [
+        'DSN'         => '',
+        'hostname'    => '127.0.0.1',
+        'username'    => '',
+        'password'    => '',
+        'database'    => ':memory:',
+        'DBDriver'    => 'SQLite3',
+        'DBPrefix'    => 'db_',
+        'pConnect'    => false,
+        'DBDebug'     => true,
+        'charset'     => 'utf8',
+        'DBCollat'    => 'utf8_general_ci',
+        'swapPre'     => '',
+        'encrypt'     => false,
+        'compress'    => false,
+        'strictOn'    => false,
+        'failover'    => [],
+        'port'        => 3306,
+        'foreignKeys' => true,
+        'busyTimeout' => 1000,
+    ];
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        if (ENVIRONMENT === 'testing') {
+            $this->defaultGroup = 'tests';
+        }
+    }
 }
-$mysqli->set_charset('utf8mb4');
