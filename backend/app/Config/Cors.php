cac1e07697d422d3d5440bbae0bbe2b6b77c209d
BISAYOT <bisayotduligas@gmail.com>
2026-05-25 18:46:53 +0800

    cors

--- backend/app/Config/Cors.php
+++ backend/app/Config/Cors.php
@@ -25,7 +25,7 @@ class Cors extends BaseConfig
      *  }
      */
     public array $default = [
-        'allowedOrigins'         => ['https://gad-ams-2.vercel.app', 'https://gadams.vercel.app/'],
+        'allowedOrigins'         => ['https://gad-ams-2.vercel.app', 'https://gadams.vercel.app'],
         'allowedOriginsPatterns' => [],
         'allowedHeaders'         => ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
         'allowedMethods'         => ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
