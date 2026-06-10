b72352609323cde25a5d2d812123141876f9e5d4
BISAYOT <bisayotduligas@gmail.com>
2026-06-10 13:07:13 +0800

    mm

--- backend/database/migrations/add_password_reset_columns.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add password reset columns to users table
-
-ALTER TABLE `users`
-ADD COLUMN `reset_token` VARCHAR(64) NULL DEFAULT NULL AFTER `password`,
-ADD COLUMN `reset_token_expires_at` DATETIME NULL DEFAULT NULL AFTER `reset_token`;
